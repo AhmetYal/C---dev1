865943c4cc6b1e073ffde9ebda0dec5bcdb574f1
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2021-12-31 09:55:23 +0300

    ikincisoru

--- Program.cs
+++ Program.cs
@@ -1,31 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace odev1
-{ 
-    //   ODEV1 1.SORU
-    //Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
+{
+    //Odev1 ikinci soru
+    //Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). 
     //Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
-    //Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
-    class birinciSoru
+    //Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+    class ikinciSoru
     {
         static void Main(string[] args)
         {
-            Console.Write("Pozitif bir sayi giriniz ");
-            int n= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0} adet sayi giriniz",n);
+            Console.Write("Pozitif iki sayi giriniz ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} adet sayi giriniz", n);
             for (int i = 0; i < n; i++)
             {
-                Console.WriteLine("{0}.Sayıyı Giriniz;", i+1);
+                Console.WriteLine("{0}.Sayıyı Giriniz;", i + 1);
                 int sayi = Convert.ToInt32(Console.ReadLine());
-                if (sayi % 2 == 0)
+                if ((sayi == m) || (sayi % m == 0))
                 {
-                    Console.WriteLine("Çift "+sayi);
+
+                    Console.WriteLine("Tam bölünüyor " + sayi);
                 }
             }
-
         }
     }
-  
-    
 }
